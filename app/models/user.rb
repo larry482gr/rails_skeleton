b16289db343fc32dbd1fc21280b1eefbdfac6da3
Lazaros Kazantzis <lakazantzis@gmail.com>
2015-10-01 01:44:30 +0300

    Added basic user schema.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   belongs_to :language
+  has_one :user_profile
+  has_many :user_passwords
+
+  has_many :login_attempts
 end
--- app/models/user_password.rb
+++ app/models/user_password.rb
@@ -1,5 +1,6 @@
 class UserPassword < ActiveRecord::Base
   belongs_to :user
-  belongs_to :service
-  belongs_to :password_question
+  # belongs_to :service
+  belongs_to :password_question, :foreign_key => 'question_1'
+  belongs_to :password_question, :foreign_key => 'question_2'
 end
