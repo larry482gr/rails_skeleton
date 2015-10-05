297f730c27e4c1cacceb6914a707e3ac6bcf0181
Lazaros Kazantzis <lk@amdtelecom.net>
2015-10-05 17:32:09 +0300

    Modified db migrations as per requirements (collation, default values).

--- db/migrate/20150930137103_create_users.rb
+++ db/migrate/20150930137103_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-      t.string :username, limit: 20
-      t.string :email, limit: 50
+    create_table :users, options: 'CHARSET=latin1 COLLATE=latin1_general_ci' do |t|
+      t.string :username, limit: 20, null: false
+      t.string :email, limit: 50, null: false
       t.string :confirmation_token, limit: 10
       t.datetime :confirmation_sent_at, null: false
       t.datetime :confirmed_at
@@ -10,14 +10,17 @@ class CreateUsers < ActiveRecord::Migration
       t.boolean :locked, default: false
       t.boolean :deleted, default: false
       t.boolean :blacklisted, default: false
-      t.references :language, index: true, foreign_key: true
+      t.references :language, default: 1
       t.integer :created_by, default: nil
 
       t.timestamps null: false
     end
 
-    add_index :users, :username, unique: true
-    add_index :users, :email, unique: true
-    add_index :users, :confirmation_token, unique: true
+    add_index :users, :username, unique: true, name: 'idx_username'
+    add_index :users, :email, unique: true, name: 'idx_email'
+    add_index :users, :confirmation_token, unique: true, name: 'idx_confirmation_token'
+
+    add_index :users, :language_id, name: 'idx_language_id'
+    add_foreign_key :users, :languages, column: :language_id, primary_key: :id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,69 +14,57 @@
 ActiveRecord::Schema.define(version: 20150930164749) do
 
   create_table "languages", force: :cascade do |t|
-    t.string   "locale",     limit: 10, null: false
-    t.string   "language",   limit: 20, null: false
-    t.boolean  "active"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.string   "locale",     limit: 10,                null: false
+    t.string   "language",   limit: 20,                null: false
+    t.boolean  "active",                default: true, null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
   end
 
-  add_index "languages", ["locale"], name: "index_languages_on_locale", unique: true, using: :btree
+  add_index "languages", ["locale"], name: "idx_locale", unique: true, using: :btree
 
   create_table "login_attempts", force: :cascade do |t|
-    t.integer  "user_id",    limit: 4
-    t.string   "ip_address", limit: 15
-    t.string   "user_agent", limit: 255
-    t.boolean  "success"
-    t.datetime "created_at",             null: false
+    t.integer  "user_id",    limit: 4,                  null: false
+    t.string   "ip_address", limit: 15,                 null: false
+    t.string   "user_agent", limit: 255,                null: false
+    t.boolean  "success",                default: true, null: false
+    t.datetime "created_at",                            null: false
   end
 
-  add_index "login_attempts", ["user_id"], name: "index_login_attempts_on_user_id", using: :btree
-
-  create_table "password_questions", force: :cascade do |t|
-    t.string   "question",   limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
-  add_index "password_questions", ["question"], name: "index_password_questions_on_question", unique: true, using: :btree
+  add_index "login_attempts", ["user_id"], name: "idx_user_id", using: :btree
 
   create_table "user_passwords", force: :cascade do |t|
-    t.integer  "user_id",    limit: 4
-    t.integer  "question_1", limit: 4
+    t.integer  "user_id",    limit: 4,   null: false
+    t.string   "password",   limit: 255, null: false
+    t.string   "question_1", limit: 255
     t.string   "answer_1",   limit: 255
-    t.integer  "question_2", limit: 4
+    t.string   "question_2", limit: 255
     t.string   "answer_2",   limit: 255
     t.datetime "expires_at"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
 
-  add_index "user_passwords", ["question_1"], name: "index_user_passwords_on_question_1", using: :btree
-  add_index "user_passwords", ["question_2"], name: "index_user_passwords_on_question_2", using: :btree
-  add_index "user_passwords", ["user_id"], name: "index_user_passwords_on_user_id", using: :btree
+  add_index "user_passwords", ["user_id"], name: "idx_user_id", using: :btree
 
   create_table "user_profiles", id: false, force: :cascade do |t|
     t.integer "user_id",         limit: 4
-    t.string  "gender",          limit: 1
-    t.string  "fname",           limit: 20
+    t.string  "gender",          limit: 1,   default: "m",   null: false
+    t.string  "fname",           limit: 30
     t.string  "lname",           limit: 30
-    t.string  "mobile",          limit: 20
+    t.string  "mobile",          limit: 20,                  null: false
     t.string  "photo",           limit: 255
-    t.text    "addresses",       limit: 65535
-    t.string  "city",            limit: 50
-    t.string  "region",          limit: 80
-    t.string  "zipcode",         limit: 20
-    t.string  "country",         limit: 100
-    t.integer "failed_attempts", limit: 1,     default: 0
+    t.boolean "failed_attempts",             default: false
   end
 
-  add_index "user_profiles", ["mobile"], name: "index_user_profiles_on_mobile", unique: true, using: :btree
-  add_index "user_profiles", ["user_id"], name: "index_user_profiles_on_user_id", using: :btree
+  add_index "user_profiles", ["fname"], name: "idx_fname", using: :btree
+  add_index "user_profiles", ["lname"], name: "idx_lname", using: :btree
+  add_index "user_profiles", ["mobile"], name: "idx_mobile", using: :btree
+  add_index "user_profiles", ["user_id"], name: "idx_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",             limit: 20
-    t.string   "email",                limit: 50
+    t.string   "username",             limit: 20,                 null: false
+    t.string   "email",                limit: 50,                 null: false
     t.string   "confirmation_token",   limit: 10
     t.datetime "confirmation_sent_at",                            null: false
     t.datetime "confirmed_at"
@@ -84,20 +72,18 @@ ActiveRecord::Schema.define(version: 20150930164749) do
     t.boolean  "locked",                          default: false
     t.boolean  "deleted",                         default: false
     t.boolean  "blacklisted",                     default: false
-    t.integer  "language_id",          limit: 4
+    t.integer  "language_id",          limit: 4,  default: 1
     t.integer  "created_by",           limit: 4
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["language_id"], name: "index_users_on_language_id", using: :btree
-  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
+  add_index "users", ["confirmation_token"], name: "idx_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "idx_email", unique: true, using: :btree
+  add_index "users", ["language_id"], name: "idx_language_id", using: :btree
+  add_index "users", ["username"], name: "idx_username", unique: true, using: :btree
 
   add_foreign_key "login_attempts", "users"
-  add_foreign_key "user_passwords", "password_questions", column: "question_1"
-  add_foreign_key "user_passwords", "password_questions", column: "question_2"
   add_foreign_key "user_passwords", "users"
   add_foreign_key "user_profiles", "users"
   add_foreign_key "users", "languages"
